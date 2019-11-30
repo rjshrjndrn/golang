ba51dcb09b839350ba60a5e66264ad44fb4a9649
rjshrjndrn <rjshrjndrn@gmail.com>
2019-11-30 09:19:52 +0530

    Updating doc

--- headerEcho_new.go
+++ headerEcho_new.go
@@ -1,3 +1,19 @@
+// This is a simple webserver runs on port 4000 and will
+// print all headers, path, and HTTPVersion version
+// of the particular request.
+//
+// Docker image is published
+// docker run --rm -p 4000:4000 rjshrjndrn/headerecho
+//
+// example: go run headerEcho_new.go
+//
+// curl localhost:4000/dummy/path -Hhello:world
+// 		"User-Agent": ["curl/7.67.0"]
+// 		"Accept": ["*/*"]
+// 		"Hello": ["world"]
+// 		"HTTPVersion": "HTTP/1.1"
+// 		"RequestPath": "/dummy/path"
+
 package main
 
 import (
@@ -24,7 +40,6 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "\"HTTPVersion\": %q\n", r.Proto)
 	fmt.Fprintf(w, "\"RequestPath\": %q", r.URL.Path)
-
 }
 
 func main() {
